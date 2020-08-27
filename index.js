df7741899207d25cd58a898ec6f441be9502b17e
kinect59 <hazem.wannous@univ-lille1.fr>
2020-08-27 18:03:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,30 +46,15 @@ run();
 
  */
 
-// Tiny TFJS train / predict example.
-async function run0() {
-  // Create a simple model.
-  const model = tf.sequential();
-  model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-  model.summary();	
-  // Prepare the model for training: Specify the loss and the optimizer.
-  model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-  // Generate some synthetic data for training. (y = 2x - 1)
-  const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-  const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
-  // Train the model using the data.
-  await model.fit(xs, ys, {epochs: 250});
-  // Use the model to do inference on a data point the model hasn't seen.
-  // Should print approximately 39.
-  document.getElementById('micro-out-div').innerText =
-      model.predict(tf.tensor2d([20], [1, 1])).dataSync();
-}
-
+/*tf.loadModel('file://./model-1a/model.json').then(model => {
+  model.predict();
+});*/
 
 // Tiny TFJS train / predict example.
 async function run() {
  
-  model = await tf.loadLayersModel('XOR/web_model/model.json')
+  model = await tf.loadModel('XOR/web_model/model.json').then(model => {
+  model.predict();
   /*const model = await tf.loadModel('./XOR/web_model/model.json', {
 	load: myCustomLoadFunc, // what is the required signature for myCustomLoadFunc?
 });*/
@@ -85,4 +70,4 @@ async function run() {
   
 }
 
-run0();
+run();
