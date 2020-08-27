c8e3e1392f31a1ad8d2acc49b81f24b5e0d3d761
kinect59 <hazem.wannous@univ-lille1.fr>
2020-08-27 15:42:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,12 +50,12 @@ run();
 async function run() {
   
   model = await tf.loadModel('XOR/web_model/model.json')
+  y = model.predict(tf.zeros([1,2])) 
+  document.getElementById('out').innerHTML = y.dataSync()[0]
+  /*
    document.getElementById('micro-out-div').innerText =
       model.predict(tf.zeros([1,2])).dataSync()[0];
-  
- /*  
- y = model.predict(tf.zeros([1,2])) 
- document.getElementById('out').innerHTML = y.dataSync()[0] */
+*/
   
 }
 
